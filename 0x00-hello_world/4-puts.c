5561739bd492cf1d19ce793a2ddbfd57a5e41754
mocyn3 <anakhalidana@gmail.com>
2023-03-16 07:13:15 -0700

    FIRST SCRIPT IN C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point/ program that prints a line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
