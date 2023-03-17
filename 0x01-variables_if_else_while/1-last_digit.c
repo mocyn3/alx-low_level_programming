115e5c9a6e854e457d2167e65e53a59515f7c12a
mocyn3 <anakhalidana@gmail.com>
2023-03-17 10:15:25 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	if (dd > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, dd);
-	}
+		}
 	else if (dd == 0)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, dd);
