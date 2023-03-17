17e338295c6848aa1372fcbc320ff4aad640cf0c
mocyn3 <anakhalidana@gmail.com>
2023-03-17 10:45:14 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 		}
 	else if (LASTDI == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, LASTDI;
+		printf("Last digit of %d is %d and is 0\n", n, LASTDI);
 	}
 	else if (LASTDI < 6)
 	{
