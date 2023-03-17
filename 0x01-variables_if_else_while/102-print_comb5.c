fc29538af3dd0240f1be7bc720226df0338418dd
mocyn3 <anakhalidana@gmail.com>
2023-03-17 13:05:23 -0700

    Last commit of the day

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Entry point/ program that prints a line
+ * Description: 'Print all possible combinations of two two-digit numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ifi;
+	int i;
+	int jfi;
+	int j;
+
+	for (ifi = 48; ifi < 58; ifi++)
+	{
+		for (i = 48; i < 58; i++)
+		{
+			j = i + 1;
+			jfi = ifi;
+			for (; jfi < 58; jfi++)
+			{
+				for (; j < 58; j++)
+				{
+					putchar(ifi);
+					putchar(i);
+					putchar(' ');
+					putchar(jfi);
+					putchar(j);
+					if (ifi != 57 || jfirst != 57 || i != 56 || j != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				j = 48;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
