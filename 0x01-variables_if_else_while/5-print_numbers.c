9209af0ea5d6da78d8abf53355ca82838ae7eb4d
mocyn3 <anakhalidana@gmail.com>
2023-03-17 11:16:05 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point/ program that prints a line
+ * Description: 'Print single digit numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9, n++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
