c7e760dabb66ff01aac371c4629c2f304be9e38d
mocyn3 <anakhalidana@gmail.com>
2023-03-21 08:16:40 -0700

    commitement

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @a: the character to be checked
+ * Return: 1 if a is a letter, 0 otherwise
+ */
+int _isalpha(int a)
+{
+	return ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'));
+}
