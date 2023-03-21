b53b54bdcc46e076fdbd473352190f40bed0c92b
mocyn3 <anakhalidana@gmail.com>
2023-03-21 08:20:24 -0700

    My commit of this task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @a: the int to check
+ * Return: 1 and prints + if a is greater than zero
+ * 0 and prints 0 if a is zero
+ * -1 and prints - if a is less than zero
+ */
+int print_sign(int a)
+{
+	if (a > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (a == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (a < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
